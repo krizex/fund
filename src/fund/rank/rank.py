e78faeb1ee529c3594544638fef3883f4014285f
krizex <krizex@gmail.com>
2017-07-06 22:21:53 +0800

    rank

--- src/fund/rank/rank.py
+++ src/fund/rank/rank.py
@@ -44,8 +44,8 @@ class FundRankBase(object):
             rank_rate_per_season = ['%3.1f%%' % (rank_rate*100) for rank_rate in rec['fund_rank_list'][:season_cnt]]
             print "%2d => %s -- %s -- %s" % (i, fund_id, fund_name, ', '.join(rank_rate_per_season))
 
-    def total_performance(self, season_cnt):
-        return lambda rec: sum(rec['fund_rank_list'][:season_cnt])
+    def total_performance(self, season_cnt, skip_season_cnt=0):
+        return lambda rec: sum(rec['fund_rank_list'][skip_season_cnt:skip_season_cnt+season_cnt])
 
 
 class FundRankByPerformance(FundRankBase):
@@ -54,16 +54,16 @@ class FundRankByPerformance(FundRankBase):
         self._ranked_js = sorted(self._ranked_js, key=self.total_performance(season_cnt), reverse=False)
 
 
-class FundRankByIncrease(FundRankBase):
-    def rank(self, season_cnt):
-        self._ranked_js = filter(lambda rec: self.rank_list_len(rec) >= season_cnt, self._ranked_js)
-        self._ranked_js = filter(self.rank_increase(season_cnt), self._ranked_js)
-        self._ranked_js = sorted(self._ranked_js, key=self.total_performance(season_cnt), reverse=False)
+class FundRankByPastIncrease(FundRankBase):
+    def rank_with_skip(self, season_cnt, skip_season_cnt):
+        self._ranked_js = filter(lambda rec: self.rank_list_len(rec) >= (skip_season_cnt + season_cnt), self._ranked_js)
+        self._ranked_js = filter(self.rank_increase(season_cnt, skip_season_cnt), self._ranked_js)
+        self._ranked_js = sorted(self._ranked_js, key=self.total_performance(season_cnt, skip_season_cnt), reverse=False)
 
-    def rank_increase(self, season_cnt):
+    def rank_increase(self, season_cnt, skip_season_cnt):
         def _inner(rec):
             rank_list = rec['fund_rank_list']
-            for i in range(season_cnt - 1):
+            for i in range(skip_season_cnt, skip_season_cnt + season_cnt - 1):
                 if rank_list[i] > rank_list[i+1]:
                     return False
 
@@ -72,9 +72,28 @@ class FundRankByIncrease(FundRankBase):
         return _inner
 
 
+class FundRankByLatestIncrease(FundRankByPastIncrease):
+    def rank(self, season_cnt):
+        self.rank_with_skip(season_cnt, 0)
+
+
+def rank_by_latest(file, season_cnt):
+    print_fund_cnt = 100
+    rank = FundRankByLatestIncrease(file)
+    rank.rank(season_cnt)
+    rank.print_fund_rank(print_fund_cnt, season_cnt)
+
+
+def rank_by_past(file, season_cnt, skip_season_cnt):
+    print_fund_cnt = 100
+    rank = FundRankByPastIncrease(file)
+    rank.rank_with_skip(season_cnt, skip_season_cnt)
+    rank.print_fund_rank(print_fund_cnt, season_cnt + skip_season_cnt)
+
+
 if __name__ == '__main__':
     file = '/home/qy/dev/fund/src/spider/items-2017-07-02-23-18-34.json'
-    rank = FundRankByIncrease(file)
-    rank.rank(4)
-    rank.print_fund_rank(100, 4)
+    # rank_by_latest(file, 4)
+    print '-' * 40
+    rank_by_past(file, 2, 1)
 
